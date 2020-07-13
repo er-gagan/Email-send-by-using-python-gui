447864175167814b381e6848a0be77e606b63fc0
Gagan Aggarwal <62302397+pythoniseasy-hub@users.noreply.github.com>
2020-07-13 12:12:36 +0530

    Adding Clear Button Functionality
    Adding Clear Button Functionality

--- Email Send By Python/main.py
+++ Email Send By Python/main.py
@@ -14,6 +14,20 @@ label_for_image.place(x=-5, y=-2)
 EmailVar = StringVar()
 SubjectVar = StringVar()
 
+def Clear():
+    EmailVar.set("")
+    SubjectVar.set("")
+    t1.delete('1.0',END)
+    
+    e1.insert(0,"Enter client email address")
+    e1.bind("<FocusIn>",lambda args: e1.delete("0",'end'))
+    
+    e2.insert(0,"Enter subject")
+    e2.bind("<FocusIn>",lambda args: e2.delete("0",'end'))
+    
+    t1.insert(END,"Enter message")
+    t1.bind("<FocusIn>",lambda args: t1.delete("1.0",END))
+
 def Send():
     emailVar = EmailVar.get()
     subjectVar = SubjectVar.get()
@@ -21,16 +35,20 @@ def Send():
     print(emailVar,subjectVar,Msg)
 
 Label(root,text="Email",font=("Calibri",25),width=8).place(x=100,y=40)
-Entry(root,font=("Calibri",25),width=25,bd=10,textvariable=EmailVar).place(x=280,y=32)
+e1 = Entry(root,font=("Calibri",25),width=25,bd=10,textvariable=EmailVar)
+e1.place(x=280,y=32)
 
 Label(root,text="Subject",font=("Calibri",25),width=8).place(x=100,y=100)
-Entry(root,font=("Calibri",25),width=25,bd=10,textvariable=SubjectVar).place(x=280,y=92)
+e2 = Entry(root,font=("Calibri",25),width=25,bd=10,textvariable=SubjectVar)
+e2.place(x=280,y=92)
 
 Label(root,text="Message",font=("Calibri",25),width=8).place(x=100,y=160)
 t1 = Text(root,height=5,width=25,font=("Calibri",25),bd=10)
 t1.place(x=280,y=152)
 
 Button(root,text="Send",font=("Calibri",22),width=6,command=Send).place(x=340,y=385)
-Button(root,text="Clear",font=("Calibri",22),width=6).place(x=500,y=385)
+Button(root,text="Clear",font=("Calibri",22),width=6,command=Clear).place(x=500,y=385)
+
+Clear()
 
 root.mainloop()
